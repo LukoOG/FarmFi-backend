bd74431f8a654362b9131b983f862921a38c86e7
unknown <emmanueladesipe01@gmail.com>
2025-03-26 06:22:49 -0700

    fixing cors agani 1

--- backend/server.js
+++ backend/server.js
@@ -14,17 +14,17 @@ const allowedOrigins = [
   
   app.use(
     cors({
-        // origin: function (origin, callback) {
-        //     // bypass the requests with no origin (like curl requests, mobile apps, etc )
-        //     if (!origin) return callback(null, true);
+        origin: function (origin, callback) {
+            // bypass the requests with no origin (like curl requests, mobile apps, etc )
+            if (!origin) return callback(null, true);
         
-        //     if (allowedOrigins.indexOf(origin) === -1) {
-        //       var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-        //       return callback(new Error(msg), false);
-        //     }
-        //     return callback(null, true);
-        //   },
-      origin: "http://localhost:3000",
+            if (allowedOrigins.indexOf(origin) === -1) {
+              var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+              return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+          },
+      // origin: "http://localhost:3000",
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE"],
       allowedHeaders: ["Content-Type", "Authorization"],
