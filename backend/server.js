106356c3b97efda85ec15f6a5f8149ef0f2d6fb5
unknown <emmanueladesipe01@gmail.com>
2025-03-26 04:29:49 -0700

    fixing inaccessibility

--- backend/server.js
+++ backend/server.js
@@ -34,4 +34,4 @@ connectDB()
 app.use("/auth", require("./routes/authRoutes"))
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, "0.0.0.0",() => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,() => console.log(`Server running on port ${PORT}`));
