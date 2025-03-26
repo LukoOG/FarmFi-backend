5521f260d3f55edee240debfa193d489d48950c7
unknown <emmanueladesipe01@gmail.com>
2025-03-26 05:57:57 -0700

    inaccessibility fixed?

--- backend/server.js
+++ backend/server.js
@@ -14,23 +14,24 @@ const allowedOrigins = [
   
   app.use(
     cors({
-        origin: function (origin, callback) {
-            // bypass the requests with no origin (like curl requests, mobile apps, etc )
-            if (!origin) return callback(null, true);
+        // origin: function (origin, callback) {
+        //     // bypass the requests with no origin (like curl requests, mobile apps, etc )
+        //     if (!origin) return callback(null, true);
         
-            if (allowedOrigins.indexOf(origin) === -1) {
-              var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-              return callback(new Error(msg), false);
-            }
-            return callback(null, true);
-          },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+        //     if (allowedOrigins.indexOf(origin) === -1) {
+        //       var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+        //       return callback(new Error(msg), false);
+        //     }
+        //     return callback(null, true);
+        //   },
+      origin: "http://localhost:3000",
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
     })
   );
 app.use(express.json());
-app.use(cookieParser);
+app.use(cookieParser());
 
 connectDB()
 setInterval(() => {
@@ -47,4 +48,4 @@ process.on("SIGINT", async () => {
   process.exit(0);
 });
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, "0.0.0.0",() => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,() => console.log(`Server running on port ${PORT}`));
