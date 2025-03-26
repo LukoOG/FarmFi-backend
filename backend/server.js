f6e0495c3f7b9f36a026f0f9d89764823fa9ee47
unknown <emmanueladesipe01@gmail.com>
2025-03-26 04:09:56 -0700

    allowing vercel on cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 const allowedOrigins = [
     "http://localhost:3000", // Dev Frontend
-    "https://your-production-frontend.com", // ✅ Production Frontend
+    "https://farm-fi.vercel.app", // ✅ Production Frontend
   ];
   
   app.use(
@@ -33,5 +33,5 @@ connectDB()
 
 app.use("/auth", require("./routes/authRoutes"))
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
