47c31f511a0674ab2e8e90e317c59b0e4a91b665
unknown <emmanueladesipe01@gmail.com>
2025-03-26 02:30:57 -0700

    added PORT to env

--- backend/server.js
+++ backend/server.js
@@ -12,5 +12,5 @@ connectDB()
 
 app.use("/auth", require("./routes/authRoutes"))
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
