c3d7b12f351abab7222404ce31741c1d40f33447
unknown <emmanueladesipe01@gmail.com>
2025-03-26 04:54:42 -0700

    fixing inaccessibiliy again

--- backend/server.js
+++ backend/server.js
@@ -14,16 +14,19 @@ const allowedOrigins = [
   
   app.use(
     cors({
-      origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error("Not allowed by CORS"));
-        }
-      },
-      credentials: true,
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      allowedHeaders: ["Content-Type", "Authorization"],
+        origin: function (origin, callback) {
+            // bypass the requests with no origin (like curl requests, mobile apps, etc )
+            if (!origin) return callback(null, true);
+        
+            if (allowedOrigins.indexOf(origin) === -1) {
+              var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+              return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+          },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     })
   );
 app.use(express.json());
@@ -34,4 +37,4 @@ connectDB()
 app.use("/auth", require("./routes/authRoutes"))
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0",() => console.log(`Server running on port ${PORT}`));
