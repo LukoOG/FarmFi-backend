7d6f95ab6701beaea1f3070d918176f4a44e20e4
unknown <emmanueladesipe01@gmail.com>
2025-03-27 06:07:07 -0700

    upload images fixup

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const upload = require("../middleware/upload")
 
 const productController = require("../controllers/productControllers")
 
-router.post("/create", productController.create_product)
+router.post("/create", upload.single("image"), productController.create_product)
 router.get("/", productController.get_All);
 router.get("/:id", productController.get_id);
 router.put("/update/:id", productController.update_product)
